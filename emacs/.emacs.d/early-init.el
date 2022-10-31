cab03aacee329fa4f611bb650157f1357cf7f835
Yue Zhu <1224535+yuezhu@users.noreply.github.com>
2022-10-31 10:31:24 -0400

    emacs: disable menu-bar-mode

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -4,9 +4,9 @@
 (setq read-process-output-max 4194304)
 (setq load-prefer-newer t)
 
-(setq tool-bar-mode nil)
-(setq scroll-bar-mode nil)
-(setq menu-bar-mode 1)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 (setq frame-title-format
       '((:eval (or buffer-file-name (buffer-name)))
         (:eval (if (buffer-modified-p) " * " " - "))
